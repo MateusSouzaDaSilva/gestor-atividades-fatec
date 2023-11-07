c3ae5fdfbfcb53130a3c743aeaf48eae716c5a4d
Mateus <mateus.ssilva2002@gmail.com>
2023-11-07 18:25:24 -0300

    status checkbox no modal atualizar

--- backend/atualizar.php
+++ backend/atualizar.php
@@ -9,10 +9,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $novaDescricao = $_POST['atualizarDescricao'];
     $novaConclusao = $_POST['atualizarConclusao'];
     $atividadeId = $_POST['atividadeId']; // Supondo que você tenha o ID da atividade a ser atualizada
+    $status = $_POST['status'];
 
     // Atualize a atividade no banco de dados
-    $statement = $conexao->prepare("UPDATE atividades SET titulo = ?, descricao = ?, data_conclusao = ? WHERE id = ?");
-    $statement->execute([$novoTitulo, $novaDescricao, $novaConclusao, $atividadeId]);
+    $statement = $conexao->prepare("UPDATE atividades SET titulo = ?, descricao = ?, data_conclusao = ?, status = ? WHERE id = ?");
+    $statement->execute([$novoTitulo, $novaDescricao, $novaConclusao, $status, $atividadeId]);
 
     // Verifique se a atualização foi bem-sucedida
     if ($statement) {
