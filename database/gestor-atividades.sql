e7029152f9ea6bd6a44fe7b636bad9fd141afaed
Mateus <mateus.ssilva2002@gmail.com>
2023-11-07 18:08:00 -0300

    database updated

--- database/gestor-atividades.sql
+++ database/gestor-atividades.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06/11/2023 às 21:24
+-- Tempo de geração: 07/11/2023 às 22:06
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -42,12 +42,18 @@ CREATE TABLE `atividades` (
 --
 
 INSERT INTO `atividades` (`id`, `data_criacao`, `titulo`, `descricao`, `data_conclusao`, `user_id`, `status`) VALUES
-(32, '2023-10-31 21:42:10', 'okata', 'testaa', '2023-11-09 03:00:00', 1, 'fechado'),
-(33, '2023-11-04 21:37:50', 'fUNFA??', 'gagadagag', '0000-00-00 00:00:00', 1, 'aberto'),
-(36, '2023-11-05 18:54:27', 'gaadf', 'gagdd', '2023-11-22 03:00:00', 1, 'aberto'),
-(39, '2023-11-06 01:37:58', 'testezaratrs', 'asdasdasfd', '2023-11-22 03:00:00', 2, 'aberto'),
-(40, '2023-11-06 01:38:44', 'aasdasdf', 'adfadagfgd', '2023-11-10 03:00:00', 2, 'aberto'),
-(41, '2023-11-06 01:44:35', 'Novo teste2', 'NOVO NOVOa', '0000-00-00 00:00:00', 2, 'aberto');
+(32, '2023-10-31 21:42:10', 'okatat', 'testaa', '0000-00-00 00:00:00', 4, 'fechado'),
+(33, '2023-11-04 21:37:50', 'fUNFA??', 'gagadagag', '0000-00-00 00:00:00', 4, 'aberto'),
+(39, '2023-11-06 01:37:58', 'testezaratrsasf', 'asdasdasfd', '0000-00-00 00:00:00', 3, 'aberto'),
+(40, '2023-11-06 01:38:44', 'aasdasdf', 'adfadagfgd', '2023-11-10 03:00:00', 3, 'aberto'),
+(42, '2023-11-06 21:09:20', 'teste closed', 'teste closedteste closed', '2023-11-10 03:00:00', 3, 'fechado'),
+(44, '2023-11-06 21:27:47', 'teste6', 'asdasd', '2023-11-01 03:00:00', 3, 'aberto'),
+(45, '2023-11-07 18:54:41', 'teste4', 'teste5', '2023-11-30 03:00:00', 3, 'aberto'),
+(46, '2023-11-07 18:54:57', 'teste5', 'teste5', '2023-11-16 03:00:00', 3, 'aberto'),
+(47, '2023-11-07 18:56:53', 'teste', 'teste', '2023-11-24 03:00:00', 4, 'aberto'),
+(48, '2023-11-07 18:57:34', 'aaaaaa', 'aaaaaa', '2023-11-01 03:00:00', 4, 'aberto'),
+(49, '2023-11-07 19:32:14', 'testeadd', 'testeadd', '2023-11-24 03:00:00', 3, 'aberto'),
+(50, '2023-11-07 19:33:11', 'rrt', 'ararara', '2023-11-02 03:00:00', 3, 'aberto');
 
 -- --------------------------------------------------------
 
@@ -69,8 +75,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `sobrenome`, `data_nascimento`, `login`, `senha`) VALUES
-(1, 'Mateus', 'Silva', '2002-09-29', 'mateus', 'teste'),
-(2, 'Cremilda', 'Fonseca', '1980-11-30', 'cremi', 'asd123');
+(2, 'Cremilda', 'Fonseca', '1980-11-30', 'cremi', 'asd123'),
+(3, 'joao', 'galvao', '1986-10-30', 'jota', '$2y$10$XXUZmy3X/.Lm9QQQ.h.Lze/eXrBNk7QzytHhcsVZdai76gyg2Vp1y'),
+(4, 'Mateus', 'Silva', '0000-00-00', 'mateus', '$2y$10$WI2zhtJZnHdWuP.yrvDeyeYSBb/pfa1SM6sbDgrZDv8xMU9y1.39q');
 
 --
 -- Índices para tabelas despejadas
@@ -97,13 +104,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `atividades`
 --
 ALTER TABLE `atividades`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
